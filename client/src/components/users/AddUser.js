426a1e9540d1693061f5c3625d9e455a53ba865b
szantilas87 <szantilas@gmail.com>
2020-02-08 07:50:04 +0000

    Prepare for deploy

--- client/src/components/users/AddUser.js
+++ client/src/components/users/AddUser.js
@@ -1,18 +1,10 @@
-import React, {
-  useState
-} from 'react';
+import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import {
-  connect
-} from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {
-  addUser
-} from '../../actions/messageActions';
+import { addUser } from '../../actions/messageActions';
 
-const AddUser = ({
-  addUser
-}) => {
+const AddUser = ({ addUser }) => {
   const [user, setUser] = useState('');
 
   const onSubmit = () => {
@@ -30,46 +22,34 @@ const AddUser = ({
       setUser('');
     }
   };
-  return ( <
-    div id = 'add-user-modal'
-    className = 'modal' >
-    <
-    div className = 'modal-content' >
-    <
-    h4 > New User < /h4> <
-    div className = 'row' >
-    <
-    div className = 'input-field' >
-    <
-    input type = 'text'
-    name = 'user'
-    value = {
-      user
-    }
-    onChange = {
-      e => setUser(e.target.value)
-    }
-    /> <
-    label htmlFor = 'user'
-    className = 'active' >
-    User Name <
-    /label> <
-    /div> <
-    /div>
-
-    <
-    div className = 'modal-footer' >
-    <
-    a href = '#!'
-    onClick = {
-      onSubmit
-    }
-    className = 'modal-close waves-effect blue waves-light btn' >
-    Enter <
-    /a> <
-    /div> <
-    /div> <
-    /div>
+  return (
+    <div id='add-user-modal' className='modal'>
+      <div className='modal-content'>
+        <h4> New User </h4>{' '}
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='user'
+              value={user}
+              onChange={e => setUser(e.target.value)}
+            />{' '}
+            <label htmlFor='user' className='active'>
+              User Name{' '}
+            </label>{' '}
+          </div>{' '}
+        </div>
+        <div className='modal-footer'>
+          <a
+            href='#!'
+            onClick={onSubmit}
+            className='modal-close waves-effect blue waves-light btn'
+          >
+            Enter{' '}
+          </a>{' '}
+        </div>{' '}
+      </div>{' '}
+    </div>
   );
 };
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
 
 var bodyParser = require('body-parser');
 var app = express();
@@ -8,7 +10,16 @@ app.use(bodyParser.json());
 
 connectDB();
 
+
 app.use('/messages', require('./routes/messages'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
